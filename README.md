8227422d78cd86c6fc4e617a07e48ece60a067a7
umarkhan <ku6205939@gmail.com>
2024-05-14 01:12:18 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**Day-74 Task:**
+
+Learn about TypeScript Debugging by using the following guide and debug along with it:
+
+[Debugging in TypeScript](./TS-debugging/README.md)
---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+/*************
+ * DEBUGGING *
+ *************/
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
